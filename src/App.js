6d5ea573b52f8f8ec48023ab0baa384502093411
Taljjaa <jenken131@gmail.com>
2019-12-11 17:03:47 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,76 @@
 import React, { Component } from 'react';
 
-import Timer from './Timer'
-import Controls from './Controls'
+import Timer from './Timer';
+import Controls from './Controls';
 
 //no need to modify anything in this component
 class App extends Component {
+   state = {
+      updateInterval: 1,
+      timerIDs: [],
+   };
 
-  state = {
-    updateInterval: 1,
-    timerIDs: []
-  }
+   componentDidMount() {
+      this.handleAddTimer();
+   }
 
-  componentDidMount() {
-    this.handleAddTimer()
-  }
+   render() {
+      console.log(this.state.timerIDs);
+      return (
+         <div className="App">
+            <header>
+               <h1>MultiTimer</h1>
+               <Controls
+                  updateIntervalSetting={this.updateIntervalSetting}
+                  updateInterval={this.state.updateInterval}
+                  handleAddTimer={this.handleAddTimer}
+               />
+            </header>
+            <div className="TimerGrid">{this.renderTimers()}</div>
+         </div>
+      );
+   }
 
-  render() {
-    console.log(this.state.timerIDs);
-    return (
-      <div className="App">
-        <header>
-          <h1>MultiTimer</h1>
-          <Controls updateIntervalSetting={this.updateIntervalSetting} updateInterval={this.state.updateInterval} handleAddTimer={this.handleAddTimer}/>
-        </header>
-        <div className="TimerGrid">
-          {this.renderTimers()}
-        </div>
+   // returns array of components written in JSX, mapped from this.state.timerIDs
+   renderTimers = () =>
+      this.state.timerIDs.map(({ id, updateInterval }) => (
+         <Timer
+            key={id}
+            id={id}
+            removeTimer={this.removeTimer}
+            updateInterval={updateInterval}
+         />
+      ));
 
-      </div>
-    );
-  }
+   // adds a random number for timer ID
+   handleAddTimer = () => {
+      this.setState(prevState => ({
+         timerIDs: [
+            ...prevState.timerIDs,
+            {
+               updateInterval: prevState.updateInterval,
+               id: Date.now(),
+            },
+         ],
+      }));
+   };
 
-  // returns array of components written in JSX, mapped from this.state.timerIDs
-  renderTimers = () => this.state.timerIDs.map(({id, updateInterval}) => <Timer key={id} id={id} removeTimer={this.removeTimer} updateInterval={updateInterval}/>)
+   // removeTimer updates state, removing any timer that matches the provided author
+   removeTimer = id => {
+      this.setState(prevState => ({
+         timerIDs: prevState.timerIDs.filter(timer => timer.id !== id),
+      }));
+   };
 
-
-  // adds a random number for timer ID
-  handleAddTimer = () => {
-    this.setState(prevState => ({
-      timerIDs: [
-        ...prevState.timerIDs,
-        {
-          updateInterval: prevState.updateInterval,
-          id: Date.now()
-        }
-      ]
-    }))
-  }
-
-  // removeTimer updates state, removing any timer that matches the provided author
-  removeTimer = id => {
-    this.setState(prevState => ({
-      timerIDs: prevState.timerIDs.filter(timer => timer.id !== id)
-    }))
-  }
-
-  updateIntervalSetting = increment => {
-    this.setState(prevState => {
-      if (prevState.updateInterval + increment <= 1) return { updateInterval: 1 }
-      return {
-        updateInterval: prevState.updateInterval + increment
-      }
-    })
-  }
-
+   updateIntervalSetting = increment => {
+      this.setState(prevState => {
+         if (prevState.updateInterval + increment <= 1)
+            return { updateInterval: 1 };
+         return {
+            updateInterval: prevState.updateInterval + increment,
+         };
+      });
+   };
 }
 
 export default App;
